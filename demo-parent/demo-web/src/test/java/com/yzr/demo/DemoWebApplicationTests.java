67e9b099b31dd159cdf36e686c5dc0b28620f047
yzr <277566174@qq.com>
2020-11-23 14:42:13 +0800

    init

---
+++ demo-parent/demo-web/src/test/java/com/yzr/demo/DemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
